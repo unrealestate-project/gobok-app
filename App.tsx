44df2e7ce72beec591bd872ae63d4a7708d6a432
vzts <yeechan.ko@gmail.com>
2020-12-31 02:12:28 +0900

    setup bugsnag

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
 import { SafeAreaView, StatusBar, Text, View } from 'react-native'
+import Bugsnag from '@bugsnag/react-native'
 import { analytics } from 'infra/Analytics'
 
 const App = () => {
   useEffect(() => {
+    Bugsnag.start()
     analytics.init()
   }, [])
   return (
