17a1284456c0bb54e1e400115ad8d2e9b7f156cd
vzts <yeechan.ko@gmail.com>
2021-01-12 22:17:53 +0900

    setup codepush js

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ import 'react-native-gesture-handler'
 import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
+import codePush from 'react-native-code-push'
 
-AppRegistry.registerComponent(appName, () => App)
+const codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_START,
+  installMode: codePush.InstallMode.ON_NEXT_RESTART,
+  mandatoryInstallMode: codePush.InstallMode.IMMEDIATE,
+}
+AppRegistry.registerComponent(appName, () => codePush(codePushOptions)(App))
