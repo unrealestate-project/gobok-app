3fccb6642f4a36156d33b344ac46bde83b80cf3c
vzts <yeechan.ko@gmail.com>
2021-01-12 23:33:29 +0900

    fix status bar height

--- src/component/LdStatusBar.tsx
+++ src/component/LdStatusBar.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { StatusBar, View } from 'react-native'
 import { COLORS } from 'infra/Colors'
 import { getStatusBarHeight } from 'react-native-iphone-x-helper'
-import { isIOS, WINDOW_HEIGHT, WINDOW_WIDTH } from 'infra/Constants'
+import { isIOS } from 'infra/Constants'
 
 export const LdStatusBar = () => {
   return (
     <View
       style={{
-        height:
-          isIOS && WINDOW_HEIGHT / WINDOW_WIDTH > 1.6
-            ? getStatusBarHeight(true)
-            : 0,
+        height: isIOS ? getStatusBarHeight(true) : 0,
         backgroundColor: COLORS.primary500,
       }}
     >
